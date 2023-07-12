586a9860b3d6e7261704265fa89fcd0bce818fd9
guluehmedova <guliehmedova19@gmail.com>
2023-07-12 12:45:38 +0400

    responsive sidebar added

--- src/components/common/MobileNavbar.jsx
+++ src/components/common/MobileNavbar.jsx
@@ -6,13 +6,81 @@ import search from '../../assets/images/mobilesearch.svg';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import left from '../../assets/images/left.svg';
+import { useState, useRef, useEffect } from 'react';
+import avatar from '../../assets/images/SidebarAvatar.svg';
+import home from '../../assets/images/home.svg';
+import catg from '../../assets/images/catg.svg';
+import hrt from '../../assets/images/hrt.svg';
+import myorders from '../../assets/images/myorders.svg';
+import e from '../../assets/images/e.svg';
+import contactus from '../../assets/images/contactus.svg';
+import about from '../../assets/images/about.svg';
 
 const MobileNavbar = () => {
     const location = useLocation();
-    console.log(location.pathname);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const sidebarRef = useRef();
+    console.log(isSidebarOpen);
+
+    useEffect(() => {
+        let handler = (event) => {
+            if (!event.target.closest('.mobile-sidebar')) {
+                setIsSidebarOpen(false);
+            };
+        };
+        document.addEventListener('mousedown', handler);
+        return () => {
+            document.addEventListener('mousedown', handler);
+        };
+    }, []);
+
+    const showSidebar = () => {
+        setIsSidebarOpen(true);
+        if (typeof window != 'undefined' && window.document) {
+            document.body.style.overflow = 'hidden';
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window != 'undefined' && window.document && isSidebarOpen === false) {
+            document.body.style.overflow = 'unset';
+        }
+    }, [isSidebarOpen])
+    
 
     return (
         <div className='mobile-navbar'>
+            {
+                isSidebarOpen && (
+                    <div className="mobile-sidebar-content">
+                        <div ref={sidebarRef} className="mobile-sidebar">
+                            <div className="top">
+                                <img src={avatar} alt="" />
+                                <span className='text'>Sign in | Register</span>
+                            </div>
+                            <div className="first-menu sidebar-menu">
+                                <Link><img src={home} alt="" />Home</Link>
+                                <Link><img src={catg} alt="" />Categories</Link>
+                                <Link><img src={hrt} alt="" />Favorites</Link>
+                                <Link><img src={myorders} alt="" />My orders</Link>
+                            </div>
+                            <div className="line"></div>
+                            <div className="second-menu sidebar-menu">
+                                <Link><img src={e} alt="" />English | USD</Link>
+                                <Link><img src={contactus} alt="" />Contact us</Link>
+                                <Link><img src={about} alt="" />About</Link>
+                            </div>
+                            <div className="line"></div>
+                            <div className='third-menu sidebar-menu'>
+                                <Link>User agreement</Link>
+                                <Link>Partnership</Link>
+                                <Link>Partnership</Link>
+                            </div>
+                        </div>
+                        <div className="blur"></div>
+                    </div>
+                )
+            }
             <div className="top">
                 <div className="left-part top-same-style">
                     {
@@ -23,7 +91,7 @@ const MobileNavbar = () => {
                             </Link>
                         ) : (
                             <>
-                                <button className='menu-btn'><img src={navMenu} alt="" /></button>
+                                <button className='menu-btn' onClick={showSidebar}><img src={navMenu} alt="" /></button>
                                 <img src={logo} alt="" className='logo' />
                             </>
                         )
